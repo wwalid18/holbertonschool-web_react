29cb1dd9d39dc79643ed393fcf578713777f0c02
wwalid18 <agar.io.annonyms@gmail.com>
2025-06-01 13:16:39 +0100

    task 1

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
